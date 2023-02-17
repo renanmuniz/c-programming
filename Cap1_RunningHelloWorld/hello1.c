fdfd336caa7bc552b8d44c8f3dfc392b684ac82c
renanmuniz <renan.merlo@paygo.com.br>
2023-02-17 00:43:03 -0300

    Capitulo 1 - Hello World

---
+++ Cap1_RunningHelloWorld/hello1.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void) {
+	printf("\nHello world!\n");
+	return 0;
+}
---
+++ Cap1_RunningHelloWorld/hello2.c
@@ -0,0 +1,13 @@
+/*
+* hello2.c
+* My first C program with comments.
+* by Renan Muniz Merlo
+* created 2023/02/17
+*/
+
+#include <stdio.h>
+
+int main(void) {
+	printf("\nHello world!\n");
+	return 0;
+}
